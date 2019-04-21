66f03f754c8e59d4e688788d1e2af755b949153f
abahrisd <abakhri.sd@gmail.com>
2019-04-21 07:12:53 +0300

    update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     isDebug: false,
     wsUrl: 'wss://api.night2stay.com/api/v2/websocket',
-    wsAuthenticate: '{"action":"login","data":{"key":"123123 ", "wlcompany": "CMPN223463HE"},"key":"4bd97223-9ad0-4261-821d-3e9ffc356e32","type":"account"}',
+    imagesHost: 'https://img1.night2stay.com',
+    wsAuthenticate: { action: 'login', data: { key: '123123 ', wlcompany: 'CMPN223463HE' }, key: '4bd97223-9ad0-4261-821d-3e9ffc356e32', type: 'account' }
 };
